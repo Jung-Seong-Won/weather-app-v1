0d3443d23fea1ce5100f620bb6ceb2579e892577
jung-seong-won <swjung@cswsoft.com>
2025-03-16 18:33:56 +0900

    a

--- App.js
+++ App.js
@@ -2,17 +2,23 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 import React, { useState } from "react";
 
+const STYLES = ["dark-content", "light-content"];
+
 export default function App() {
   const [number, setNumber] = useState(0);
+  const [hidden, setHidden] = useState(true);
 
   return (
     <View style={styles.container}>
+      <StatusBar backgroundColor="red" hidden={hidden} />
       <Text style={{ fontSize: 50 }}>안녕 : {number}</Text>
-      <View style={{ flexDirection: "row", gap: 10 }}>
+      <View style={{ flexDirection: "row", gap: 10, margin: 20 }}>
         <Button title="더하기" onPress={() => setNumber(number + 1)} />
         <Button title="빼기" onPress={() => setNumber(number - 1)} />
       </View>
-      <StatusBar style="auto" />
+      <View style={{ margin: 20 }}>
+        <Button title="상태메뉴토글" onPress={() => setHidden(!hidden)} />
+      </View>
     </View>
   );
 }
